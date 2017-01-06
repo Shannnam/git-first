758ec3cd6488211b581c826241a3ecfaa1918c13
shannnam <2545191156@qq.com>
2017-01-06 14:51:31 +0800

    fix

--- src/com/wjw/git/File.java
+++ src/com/wjw/git/File.java
@@ -4,5 +4,6 @@ public class File {
      public static void main(String[] args){
     	 System.out.print("第一次修改文件");
     	 System.out.print("第二次修改文件");
+    	 System.out.print("第二次修改文件222222");
      }
 }
