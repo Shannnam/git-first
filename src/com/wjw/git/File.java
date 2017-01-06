ad9ac7a4248ffbf5dfc775dbe7480497a3c69240
shannnam <2545191156@qq.com>
2017-01-06 15:14:39 +0800

    third modify

--- src/com/wjw/git/File.java
+++ src/com/wjw/git/File.java
@@ -6,6 +6,9 @@ public class File {
     	 System.out.print("第二次修改文件");
     	 System.out.print("第二次修改文件222222");
     	 System.out.print("紧急修复bug,修复成功");
+    	 System.out.print("开始工作了。。。。。");
+    	 System.out.print("happy工作了。。。。。");
+
 
      }
 }
